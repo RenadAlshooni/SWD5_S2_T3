5fd16d9fe2352169ed2e14f64df62bb46c842a1e
mahmoud <mahmoudelmokaber4@gmail.com>
2025-04-24 21:31:37 +0200

    Login and signup pages added

--- TechXpress/TechXpress/Controllers/HomeController.cs
+++ TechXpress/TechXpress/Controllers/HomeController.cs
@@ -17,13 +17,16 @@ namespace TechXpress.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Signup()
         {
+
+            return View();
+        } 
+        public IActionResult Login()
+        {
+
             return View();
         }
-
-        
 
     }
 
